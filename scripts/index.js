10101f1a9891f89bef2e97baae4e97559ce3202f
yash-2507 <imyash.654@gmail.com>
2022-08-22 09:09:09 +0530

    Small changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,34 @@
-const i1 = "./tinified/spiderman.jpg"
-const i2 = "./tinified/hogwarts.jpg"
-const i3 = "./tinified/john_snow.jpg"
-const i4 = "./tinified/justice_league.jpg"
-const i5 = "./tinified/pirates.jpg"
-const i6 = "./tinified/marvel.jpg"
+const i1 = './tinified/spiderman.jpg';
+const i2 = './tinified/hogwarts.jpg';
+const i3 = './tinified/john_snow.jpg';
+const i4 = './tinified/justice_league.jpg';
+const i5 = './tinified/pirates.jpg';
+const i6 = './tinified/marvel.jpg';
 
-let arr = []
-arr.push(i1)
-arr.push(i2)
-arr.push(i3)
-arr.push(i4)
-arr.push(i5)
-arr.push(i6)
+let arr = [];
+arr.push(i1);
+arr.push(i2);
+arr.push(i3);
+arr.push(i4);
+arr.push(i5);
+arr.push(i6);
 
-
-let container = document.getElementById('slideshow')
-let img = document.createElement('img')
-img.src = arr[0]
-container.append(img)
-let i = 1
-setInterval(function() {
-    if(i === arr.length) {
-        i = 0
+let container = document.getElementById('slideshow');
+let img = document.createElement('img');
+img.src = arr[0];
+container.append(img);
+let i = 1;
+setInterval(function () {
+    if (i === arr.length) {
+        i = 0;
     }
-    let image = arr[i]
-    let img = document.createElement('img')
-    img.src = image
-    container.innerHTML = null
-    container.append(img)
-    i++
-},3000)
-
+    let image = arr[i];
+    let img = document.createElement('img');
+    img.src = image;
+    container.innerHTML = null;
+    container.append(img);
+    i++;
+}, 3000);
 
 // Movies Display
 
@@ -87,72 +85,75 @@ Harry Potter and the Deathly Hallows: Part 2 (2011)
 8.2
 PG-13 | 130 min | Adventure, Fantasy, Mystery
 https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UX140_CR0,0,140,209_AL_.jpg
-`
+`;
 
-stupid = []
+stupid = [];
 
-data.split('\n\n').forEach(movie => {
+data.split('\n\n').forEach((movie) => {
     // let movie_name, rating, description, link
 
-    let [movie_name, rating, description, link] = movie.trim().split('\n')
+    let [movie_name, rating, description, link] = movie.trim().split('\n');
     // console.log(movie_name)
 
     movie_data = {
-        "name": movie_name,
-        "rating": rating,
-        "description": description,
-        "link": link
-    }
+        name: movie_name,
+        rating: rating,
+        description: description,
+        link: link,
+    };
 
-    stupid.push(movie_data)
-
-})
+    stupid.push(movie_data);
+});
 
 // console.log(stupid)
-localStorage.setItem('movies',JSON.stringify(stupid))
+localStorage.setItem('movies', JSON.stringify(stupid));
 
+let moviesContainer = document.querySelector('#movies');
 
-let moviesContainer = document.querySelector('#movies')
+const appendData = (data) => {
+    moviesContainer.innerHTML = null;
+    data.forEach(function (elem) {
+        let movieName = elem.name;
+        let movieRating = elem.rating;
+        let movieDescription = elem.description;
+        let moviePoster = elem.link;
 
+        let img = document.createElement('img');
+        img.src = moviePoster;
+        let title = document.createElement('h3');
+        title.innerText = movieName;
+        let rate = document.createElement('p');
+        rate.innerText = movieRating;
+        let des = document.createElement('h5');
+        des.innerText = movieDescription;
 
-stupid.forEach(function(elem) {
-    let movieName = elem.name;
-    let movieRating = elem.rating;
-    let movieDescription = elem.description;
-    let moviePoster = elem.link;
+        let parentImg = document.createElement('div');
 
-    let img = document.createElement('img')
-    img.src = moviePoster;
-    let title = document.createElement('h3')
-    title.innerText = movieName;
-    let rate = document.createElement('p')
-    rate.innerText = movieRating
-    let des = document.createElement('h5')
-    des.innerText = movieDescription;
-    
-    let parentImg = document.createElement('div')
-    
-    let parentBody = document.createElement('div')
-    parentBody.append(title,rate,des)
-    parentBody.setAttribute('id','parentBody');
-    parentImg.append(img,parentBody)
-    // parentImg.classList.add('')
-    parentImg.setAttribute('id','parentImg');
-    // parentImg.style.display = 'flex'
-    // parentImg.style.gap = '20px'
-    moviesContainer.append(parentImg)
+        let parentBody = document.createElement('div');
+        parentBody.append(title, rate, des);
+        parentBody.setAttribute('id', 'parentBody');
+        parentImg.append(img, parentBody);
+        parentImg.setAttribute('id', 'parentImg');
+        moviesContainer.append(parentImg);
+    });
+};
 
-})
+appendData(stupid);
 
-
-const rating = document.querySelectorAll('#parentBody>p')
-// console.log(rating)
-for(let i = 0; i < rating.length; i++) {
-    let rate = rating[i].innerText
-    // console.log(rate)
-}
-
-let selectValue = document.getElementById('select')
-if(selectValue.value === 'Low') {
-    console.log('done')
-}
+const select = document.getElementById('select');
+select.addEventListener('change', (e) => {
+    // console.log(e.target.value);
+    let sortedData = [];
+    const sortBy = e.target.value;
+    if (sortBy === 'htl') {
+        sortedData = [...stupid];
+        sortedData.sort((a, b) => b.rating - a.rating);
+        appendData(sortedData);
+    } else if (sortBy === 'lth') {
+        sortedData = [...stupid];
+        sortedData.sort((a, b) => a.rating - b.rating);
+        appendData(sortedData);
+    } else {
+        appendData(stupid);
+    }
+});
